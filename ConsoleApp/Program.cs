5e120ec0a5946aa1dd59fafa70480f378283230a
LiCheng-SyS <1185495990@qq.com>
2023-02-23 10:35:04 +0800

    1
    11

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,35 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
     class Program
     {
+        static ActionExecutor executor = new ActionExecutor();
         static void Main(string[] args)
         {
 
-            double d = -2.345;
-            int i = (int)Convert.ChangeType(d, typeof(int));
-            var xx=  Convert.ChangeType(d, typeof(int));
-            Console.WriteLine("The double value {0} when converted to an int becomes {1}", d, i);
 
+        }
+        static void MemoryRun()
+        {
+            executor.AddAction(() => Console.WriteLine("Action 1"));
+            executor.AddAction(() => Console.WriteLine("Action 2"));
+            executor.AddAction(() => Console.WriteLine("Action 3"));
+            executor.AddAction(() => Console.WriteLine("Action 4"));
+            executor.AddAction(() => throw new Exception());
+            executor.AddAction(() => Console.WriteLine("Action 5"));
 
-
-
-            //ActionExecutor executor = new ActionExecutor();
-            //executor.AddAction(() => Console.WriteLine("Action 1"));
-            //executor.AddAction(() => Console.WriteLine("Action 2"));
-            //executor.AddAction(() => Console.WriteLine("Action 3"));
-            //executor.AddAction(() => Console.WriteLine("Action 4"));
-            //executor.AddAction(() => throw new Exception());
-            //executor.AddAction(() => Console.WriteLine("Action 5"));
-
-            //while (true)
-            //{
-            //    executor.ExecuteActions();
-            //    Thread.Sleep(1000); // 休眠1秒
-            //}
+            while (true)
+            {
+                executor.ExecuteActions();
+                Thread.Sleep(1000); // 休眠1秒
+            }
         }
     }
 }
